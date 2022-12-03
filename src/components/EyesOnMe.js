739d39b1b31952c57ff8bbee25b0457cdbde5b53
root <root@DESKTOP-JFEPESN.localdomain>
2022-12-03 21:19:50 +0200

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function handleEyesOnMeOnFocus(){
+        console.log('Good!')
+    }
+    function handleEyesOnMeOnBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <>
+        <button onFocus={handleEyesOnMeOnFocus} onBlur={handleEyesOnMeOnBlur}>Eyes on me</button>
+        </>
+    )
+}
+export default EyesOnMe
